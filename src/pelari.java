853d732f9fa5f209198bed9c69751b2d51714a82
ade <Darmawan.Ade98@yahoo.com>
2016-03-09 22:00:52 +0700

    tambah class pelari

--- src/pelari.java
+++ src/pelari.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bab1;
-
-/**
- *
- * @author AD
- */
-public class pelari {
-    
+class pelari {
+    private String nama, nim;
+    private int s, t;
+    public pelari(String nm, String nimp, int munyer, int waktu) {
+        nama = nm;
+        nim = nimp;
+        s = munyer;
+        t = waktu;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int v() {
+        return s / t;
+    }
+    public boolean seleksi(pelari zzz) {
+        return (v() > zzz.v()) ? true : false;
+    }
+    public void showData(lintasan lintasan) {
+        System.out.printf("%s %.0f\n", nama, lintasan.getK() * s);
+    }
 }
